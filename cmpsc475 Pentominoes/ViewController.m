da676f1a36eb24fa14f0a6a182fea87bda7dd3b8
BRIAN J GOLDEN <bwg5079@waring107-lab-mac148.clc-labs.its.psu.edu>
2013-09-10 17:21:30 -0400

    create playing piece image views, load in subviews (not yet placed in proper positions)

--- cmpsc475 Pentominoes/ViewController.m
+++ cmpsc475 Pentominoes/ViewController.m
@@ -8,11 +8,12 @@
 
 #import "ViewController.h"
 
-#define kNumberBoardImages 6
+#define kNumberBoardImages   6
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *Board;
 @property (strong, nonatomic) NSArray *boardImages;
+@property (strong, nonatomic) NSArray *playingPieceImageViews;
 @property NSInteger currentBoardNumber;
 - (IBAction)BoardButtonPressed:(UIButton *)sender;
 - (IBAction)ResetPressed:(UIButton *)sender;
@@ -24,12 +25,20 @@
 
 static NSString *kBoardImagePrefix = @"Board";
 static NSString *kBoardImageFileExtension = @"png";
+static NSString *kPlayingPieceImagePrefix = @"tile";
+static NSString *kPlayingPieceImageFileExtension = @"png";
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     _boardImages = [self createBoardImages];
+    _playingPieceImageViews = [self createPlayingPieceImageViews];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [self placePlayingPiecesInStartPositions];
 }
 
 - (void)didReceiveMemoryWarning
@@ -50,7 +59,13 @@ static NSString *kBoardImageFileExtension = @"png";
 
 - (IBAction)SolvePressed:(UIButton *)sender {
 }
-                    
+
+- (void) placePlayingPiecesInStartPositions {
+    for (UIImageView *playingPiece in self.playingPieceImageViews) {
+        [self.view addSubview:playingPiece];
+    }
+}
+
 - (NSArray *) createBoardImages {
     NSMutableArray *newBoardImages = [[NSMutableArray alloc] init];
     NSBundle *bundle = [NSBundle mainBundle];
@@ -63,6 +78,24 @@ static NSString *kBoardImageFileExtension = @"png";
     }
     
     return newBoardImages;
+}
+
+- (NSArray *) createPlayingPieceImageViews {
+    NSArray *tileNames = @[@"F", @"I", @"L", @"N", @"P", @"T", @"U", @"V", @"W", @"X", @"Y", @"Z"];
+    NSMutableArray *playingPieces = [[NSMutableArray alloc] init];
+    
+    for (NSString *tileName in tileNames) {
+        NSString *playingPieceImageFileName = [[NSString alloc] initWithFormat:@"%@%@.%@", kPlayingPieceImagePrefix, tileName, kPlayingPieceImageFileExtension];
+        UIImage *playingPieceImage = [UIImage imageNamed:playingPieceImageFileName];
+        UIImageView *playingPieceImageView = [[UIImageView alloc] initWithImage:playingPieceImage];
+        
+        CGRect frame = CGRectMake(0.0, 0.0, playingPieceImage.size.width / 2, playingPieceImage.size.height / 2);
+        playingPieceImageView.frame = frame;
+        
+        [playingPieces addObject:playingPieceImageView];
+    }
+    
+    return playingPieces;
 }
 
 @end
